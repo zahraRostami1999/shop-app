d7dd1812936af32e8f496e06bf3f7b071228d50d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-16 20:12:14 +0330

    edit App.jsx: set HomePage and CartPage route

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -1,16 +1,18 @@
-import Header from './components/header/Header';
-import Products from './components/products/Products';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
-
-
   return (
     <>
-      <Header />
-      <Products />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/cart" element={<CartPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
