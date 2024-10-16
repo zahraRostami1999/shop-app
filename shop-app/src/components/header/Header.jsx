a964d3ac2780fbc05c7beca0b3bafafb6362c991
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-16 14:01:30 +0330

    add Header.jsx: define Header component

---
+++ shop-app/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+
+  return (
+    <>
+      <h1 className="text-slate-700 bg-red-400 p-5 text-center font-mono font-extrabold text-3xl">
+        HELLO
+      </h1>
+    </>
+  );
+}
+
+export default Header;
