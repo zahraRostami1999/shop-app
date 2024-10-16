8da4ea0f63bd8d8bff05fe043c5d241bdcf6cb5b
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-16 20:28:59 +0330

    edit Card component: change card Title position

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -1,14 +1,13 @@
-
-function Card(){
-    return(
-        <>
-        <div className="border-red-700 border col-span-3  my-10 mx-7 p-5 rounded-tl-xl">
-            <img src="https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg" />
-            <h2>Card Title</h2>
-            <p>Card Content</p>
-        </div>
-        </>
-    );
+function Card() {
+  return (
+    <>
+      <div className="border-red-700 border col-span-3  my-10 mx-7 p-5 rounded-tl-xl">
+        <h2 className="text-center my-4 text-lg font-sans font-bold">Card Title</h2>
+        <img src="https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg" />
+        <p>Card Content</p>
+      </div>
+    </>
+  );
 }
 
 export default Card;
