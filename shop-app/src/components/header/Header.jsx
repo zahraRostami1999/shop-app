8a11571db92e05063dee6fa5729d47119b139844
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-16 21:14:23 +0330

    edit Header component: add cart Icon and set styles bg red and translate

--- shop-app/src/components/header/Header.jsx
+++ shop-app/src/components/header/Header.jsx
@@ -1,10 +1,20 @@
-function Header() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
+function Header() {
   return (
     <>
-      <h1 className="text-slate-700 bg-red-400 p-5 text-center font-mono font-extrabold text-3xl">
-        WELLCOME TO YOUR SHOP
-      </h1>
+      <div className="flex justify-between text-slate-700 bg-red-400 p-5 text-center font-mono font-extrabold text-3xl">
+        <h1>WELLCOME TO YOUR SHOP</h1>
+        <div className="flex">
+        <FontAwesomeIcon
+          icon={faShoppingCart}
+          size="lg"
+        />
+        <div className="bg-red-600 rounded-3xl text-xs font-thin h-5 px-2 py-0.5 inline -translate-x-3 -translate-y-2 cursor-pointer">0</div>
+        </div>
+
+      </div>
     </>
   );
 }
