141002bd56c2ac7cddecb68af1e5df45527af783
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-16 20:23:08 +0330

    edit HomePage: add Header and product components to the page

---
+++ shop-app/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/Header";
+import Products from "../components/products/Products";
+
+function HomePage(){
+  return (
+    <>
+      <Header />
+      <Products />
+    </>
+  );
+};
+
+export default HomePage;
