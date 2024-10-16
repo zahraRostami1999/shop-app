5475480822f9ef1e17936d4d7001b87ba2380ee2
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-16 21:12:59 +0330

    edit App.jsx: add Header

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/cart" element={<CartPage />}></Route>
