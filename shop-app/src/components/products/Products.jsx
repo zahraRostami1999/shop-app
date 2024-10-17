8dcee19dde3abeef4cf8cf62546fda3557603df3
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-17 14:19:25 +0330

    edit Product component: call API with axios

--- shop-app/src/components/products/Products.jsx
+++ shop-app/src/components/products/Products.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Card from "../card/Card";
+import axios from "axios";
 
 function Products() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/ProductAPI")
+      .then((result) => {
+        console.log(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <div className="bg-red-100 pt-20 p-10 grid grid-cols-9 gap-12">
