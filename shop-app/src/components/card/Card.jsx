61a1c267f3f4930562705db0bade188459ea5a2d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-17 14:18:53 +0330

    edit Card component: add shadow and change border color

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 function Card() {
   return (
     <>
-      <div className="border-red-700 border col-span-3  my-10 mx-7 p-5 rounded-tl-xl">
+      <div className="border-slate-300 shadow-lg border col-span-3  my-10 mx-7 p-5 rounded-lg">
         <h2 className="text-center my-4 text-lg font-sans font-bold">Card Title</h2>
         <img src="https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg" />
         <p>Card Content</p>
