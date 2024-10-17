a689a816db68e963b65ab5ab256a08bc06d30cec
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-17 14:53:13 +0330

    edit main.jsx: import Store redux and provider

--- shop-app/src/main.jsx
+++ shop-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { Store } from './redux/Store.jsx';
+import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={Store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
