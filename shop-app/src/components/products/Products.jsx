a871feec31dbc93808d674523d410d389b3d2c2e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-17 15:30:25 +0330

    edit Product component: add products slice and put product info from db to products slice

--- shop-app/src/components/products/Products.jsx
+++ shop-app/src/components/products/Products.jsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
+import {useSelector, useDispatch} from 'react-redux';
 import Card from "../card/Card";
 import axios from "axios";
+import {addProduct} from '../../redux/ProductSlice';
 
 function Products() {
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch()
+  
   useEffect(() => {
     axios
       .get("http://localhost:8000/ProductAPI")
       .then((result) => {
-        console.log(result.data);
+        dispatch(addProduct(result.data)); 
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+  console.log(products);
 
   return (
     <>
