df733ac56c3814ee64ccc153b87ddd3d49b9fd02
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-20 12:13:18 +0330

    edit Card component: add link for showing products details in Product Page

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -1,10 +1,13 @@
+import {Link} from 'react-router-dom';
+
 function Card() {
   return (
     <>
       <div className="border-slate-300 shadow-lg border col-span-3  my-10 mx-7 p-5 rounded-lg">
         <h2 className="text-center my-4 text-lg font-sans font-bold">Card Title</h2>
-        <img src="https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg" />
+        <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" />
         <p>Card Content</p>
+        <Link to={`Product/${1}`}>View Details</Link>
       </div>
     </>
   );
