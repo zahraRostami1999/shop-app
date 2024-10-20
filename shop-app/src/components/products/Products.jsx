3d54ad4fc438b13e204f9309d950fbf7bcaac88d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-20 12:38:38 +0330

    edit Product component: add Link for each product and put result of api call to ProductData var

--- shop-app/src/components/products/Products.jsx
+++ shop-app/src/components/products/Products.jsx
@@ -2,31 +2,28 @@ import { useEffect, useState } from "react";
 import Card from "../card/Card";
 import axios from "axios";
 import { getProducts } from "../../services/api";
-
+import { Link } from "react-router-dom";
 
 function Products() {
-
-
   const [productData, setProductData] = useState([]);
-  
+
   useEffect(() => {
     getProducts()
-
-      // .then((result) => {
-
-
-      // })
-      // .catch((error) => {
-      //   console.log(error);
-      // });
+      .then((result) => {
+        setProductData(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-
   return (
     <>
-      <div className="bg-red-100 pt-20 p-10 grid grid-cols-9 gap-12">
-        <Card />
-        <Card />
-        <Card />
+      <div className="bg-red-100 pt-20 p-10 grid grid-cols-3 gap-12">
+        {productData.map((product) => (
+          <Link to={`/products/${1}`}>
+            <Card key={product.id} />
+          </Link>
+        ))}
       </div>
     </>
   );
