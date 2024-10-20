b563d13b767601d3ee8b52516ffc6fdd43baeb05
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-20 23:15:06 +0330

    edit api.jsx: add function for get product details

--- shop-app/src/services/api.jsx
+++ shop-app/src/services/api.jsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: 'http://localhost:8000'
-})
+  baseURL: "http://localhost:8000",
+});
 
-export async function getProducts(){
-    const {data} = await client('/Products');
-    return data;
+export async function getProducts() {
+  const { data } = await client("/Products");
+  return data;
 }
 
+export async function getProductDetails(id) {
+  const { data } = await client(`/Products/${id}`);
+  
+  return data;
+}
