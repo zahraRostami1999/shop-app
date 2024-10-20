8960856b0908d4c06d957db64cb5338f6b035927
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-20 23:17:38 +0330

    edit Product Page: set useEffect for getProducts details

--- shop-app/src/pages/ProductPage.jsx
+++ shop-app/src/pages/ProductPage.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProductDetails } from "../services/api";
 
 function ProductPage() {
+  const Params = useParams();
+  const [productDetails, setProductDetails] = useState({});
+
+  useEffect(() => {
+    getProductDetails(Params.id).then((result) => {
+      setProductDetails(result);
+    });
+  }, []);
+
   return (
     <>
-      <h1>This is a product page</h1>
+      <div className="bg-slate-200 mx-auto my-5 pb-5 w-5/12">
+        <h1 className="text-2xl font-bold text-center py-3">
+          {productDetails.title}
+        </h1>
+        <img
+          src={productDetails.image}
+          alt={productDetails.title}
+          className=" w-1/2 h-96 mx-auto my-3"
+        />
+        <h2 className="text-lg font-bold text-center">
+          {productDetails.price} $
+        </h2>
+        <h3 className="text-base px-10">
+        {productDetails.description}
+        </h3>
+        <button className="bg-green-500 px-16 py-2 rounded-md  my-5 mx-52">
+            Buy Now
+          </button>
+      </div>
     </>
   );
 }
