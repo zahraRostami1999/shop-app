b3b51f9b97168b2aa89e436e139f98a51cd5ec07
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-21 10:50:28 +0330

    edit Store.jsx: import and add cartSlice

--- shop-app/src/redux/Store.jsx
+++ shop-app/src/redux/Store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { ProductSlice } from "./ProductSlice";
+import { CartSlice } from "./CartSlice";
 
 export const Store = configureStore({
   reducer: {
-    products: ProductSlice.reducer,
+    cart: CartSlice.reducer,
   },
 });
