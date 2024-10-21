febb7603e27145bfba8940f0f2a0072347b5d9b9
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-21 10:49:54 +0330

    create CartSlice

---
+++ shop-app/src/redux/CartSlice.jsx
@@ -0,0 +1,11 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        cartItem : []
+    },
+    reducers:{
+
+    }
+});
