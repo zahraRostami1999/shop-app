3c394d01fb213411a6625cd8990c2bc88ec9bd23
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-21 18:40:10 +0330

    edit ProductSlice: sort Slice

--- shop-app/src/redux/ProductSlice.jsx
+++ shop-app/src/redux/ProductSlice.jsx
@@ -1,19 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const ProductSlice = createSlice(
-    {
-        name: "products",
-        initialState:{
-            items:[]
-        },
-        reducers:{
-            addProduct: (state, action) => {
-                return {
-                ...state,
-                items: [...state.items, action.payload]
-              };
-        }
+export const ProductSlice = createSlice({
+  name: "products",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addProduct: (state, action) => {
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
     },
+  },
 });
 
 export const { addProduct } = ProductSlice.actions;
