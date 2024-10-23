83fbefe765be075f45810d89f6b569f4f737102e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-23 16:28:28 +0330

    edit cart Page: add function for display cartItems

--- shop-app/src/pages/CartPage.jsx
+++ shop-app/src/pages/CartPage.jsx
@@ -4,33 +4,58 @@ import { getProducts } from "../services/api";
 
 function CartPage() {
   const { cartItem } = useSelector((state) => state.cart);
+
   const [productItems, setProductItems] = useState([]);
 
   useEffect(() => {
     getProducts().then((result) => {
       setProductItems(result);
     });
-  }, []);
+  }, [getProducts]);
 
-  
+  const displayCartItems = () => {
+    cartItem.map((item) => {
+      productItems.map((product) => {
+        if (item.id === product.id) {
+          <div key={product.id}>
+            <h2 className="mt-56">{product.id}</h2>
+          </div>;
+        } else {
+          return <h2 className="mt-56">nothing</h2>;
+        }
+      });
+    });
+  };
 
   return (
     <>
-      <h1>This is Cart Page</h1>
-      {/* <div>
-        {cartItems.length > 0 ? (
-          cartItems.map((item) => (
-            <div key={item.id} className="border p-4 mb-2 rounded-lg shadow">
-              <img src={item.image} alt={item.title} className="w-20 h-20" />
-              <h2 className="font-bold">{item.title}</h2>
-              <p>Price: {item.price}$</p>
-              <p>{item.description}</p>
+      <h1 className="mt-20">This is Cart Page</h1>
+      <div>
+        {cartItem.length > 0 ? displayCartItems() : <h1>Cart is Empty</h1>}
+      </div>
+      <div className="bg-emerald-400 p-6 my-10 mx-20 rounded-md">
+        <h1>Your Cart:</h1>
+        <ul>
+          <li className="my-5 p-3 border rounded">
+            <div className="flex items-center mb-5">
+              <img
+                className="w-36 h-32 bg-cover "
+                src="https://m.media-amazon.com/images/I/61ZJlxRC2ZL._AC_UY900_.jpg"
+              />
+
+              <div className="grid grid-cols-2 mx-10 px-3 grid-rows-4">
+                <h2 className="row-start-1 font-bold">Product Name</h2>
+                <input
+                  className="row-start-4 w-12 h-8 rounded text-center bg-teal-100"
+                  type="number" name="qty" min="1" max="20" 
+                />
+                <p className="row-start-3 font-semibold">Price: $100</p>
+              </div>
             </div>
-          ))
-        ) : (
-          <p>Your cart is empty.</p>
-        )}
-      </div> */}
+          </li>
+          
+        </ul>
+      </div>
     </>
   );
 }
--- shop-app/src/redux/CartSlice.jsx
+++ shop-app/src/redux/CartSlice.jsx
@@ -7,10 +7,22 @@ export const CartSlice = createSlice({
   },
   reducers: {
     AddToCart: (state, action) => {
-      return {
-        ...state,
-        cartItem: [...state.cartItem, action.payload],
-      };
+      let selected = state.cartItem.find(
+        (item) => item.id === parseInt(action.payload)
+      );
+      if (selected === undefined) {
+        return {
+          ...state,
+          cartItem: [...state.cartItem, { id: parseInt(action.payload), qty: 1 }],
+        };
+      } else {
+        state.cartItem = state.cartItem.map((item) => {
+          if (item.id === parseInt(action.payload)) {
+            return { ...item, qty: item.qty + 1 };
+          }
+          return item;
+        });
+      }
     },
   },
 });
