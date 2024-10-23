18e2a30368477d1620a86db6dadc4f55b46a35ae
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-23 17:46:37 +0330

    edit CartPage: remove parent extra div

--- shop-app/src/pages/CartPage.jsx
+++ shop-app/src/pages/CartPage.jsx
@@ -20,29 +20,25 @@ function CartPage() {
       );
       if (matchingProduct) {
         return (
-          <li key={matchingProduct.id} className=" rounded my-9 mx-6">
-            <div className="flex items-center">
-              <img
-                className="w-32 h-28 bg-cover "
-                src={matchingProduct.image}
-              />
+          <li
+            key={matchingProduct.id}
+            className=" rounded my-9 mx-6 flex items-center"
+          >
+            <img className="w-32 h-28 bg-cover " src={matchingProduct.image} />
 
-              <div className="grid grid-cols-2 grid-rows-4 ml-6">
-                <h2 className="row-start-1 font-bold">
-                  {matchingProduct.title}
-                </h2>
-                <input
-                  className="row-start-4 w-12 h-8 rounded text-center border"
-                  type="number"
-                  name="qty"
-                  min="1"
-                  max="20"
-                  value={item.qty}
-                />
-                <p className="row-start-3 font-semibold">
-                  Price: {matchingProduct.price} $
-                </p>
-              </div>
+            <div className="grid grid-cols-2 grid-rows-4 ml-6">
+              <h2 className="row-start-1 font-bold">{matchingProduct.title}</h2>
+              <input
+                className="row-start-4 w-12 h-8 rounded text-center border"
+                type="number"
+                name="qty"
+                min="1"
+                max="20"
+                value={item.qty}
+              />
+              <p className="row-start-3 font-semibold">
+                Price: {matchingProduct.price} $
+              </p>
             </div>
           </li>
         );
@@ -61,7 +57,11 @@ function CartPage() {
       <div></div>
       <div className=" border h-full shadow-lg mt-28 mb-10 mx-6 rounded-md">
         <ul>
-          {cartItem.length > 0 ? displayCartItems() : <h1 className="text-center py-6 " >Cart is Empty</h1>}
+          {cartItem.length > 0 ? (
+            displayCartItems()
+          ) : (
+            <h1 className="text-center py-6 ">Cart is Empty</h1>
+          )}
         </ul>
       </div>
     </>
