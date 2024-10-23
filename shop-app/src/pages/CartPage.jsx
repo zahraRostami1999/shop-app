a4c0d93347dfeed6dabae5f97ce972aa387d4374
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-23 17:43:20 +0330

    edit cartPage: display items in cart

--- shop-app/src/pages/CartPage.jsx
+++ shop-app/src/pages/CartPage.jsx
@@ -11,49 +11,57 @@ function CartPage() {
     getProducts().then((result) => {
       setProductItems(result);
     });
-  }, [getProducts]);
+  }, []);
 
   const displayCartItems = () => {
-    cartItem.map((item) => {
-      productItems.map((product) => {
-        if (item.id === product.id) {
-          <div key={product.id}>
-            <h2 className="mt-56">{product.id}</h2>
-          </div>;
-        } else {
-          return <h2 className="mt-56">nothing</h2>;
-        }
-      });
+    return cartItem.map((item) => {
+      const matchingProduct = productItems.find(
+        (product) => item.id == product.id
+      );
+      if (matchingProduct) {
+        return (
+          <li key={matchingProduct.id} className=" rounded my-9 mx-6">
+            <div className="flex items-center">
+              <img
+                className="w-32 h-28 bg-cover "
+                src={matchingProduct.image}
+              />
+
+              <div className="grid grid-cols-2 grid-rows-4 ml-6">
+                <h2 className="row-start-1 font-bold">
+                  {matchingProduct.title}
+                </h2>
+                <input
+                  className="row-start-4 w-12 h-8 rounded text-center border"
+                  type="number"
+                  name="qty"
+                  min="1"
+                  max="20"
+                  value={item.qty}
+                />
+                <p className="row-start-3 font-semibold">
+                  Price: {matchingProduct.price} $
+                </p>
+              </div>
+            </div>
+          </li>
+        );
+      } else {
+        return (
+          <h2 className="mt-56" key={item.id}>
+            nothing
+          </h2>
+        );
+      }
     });
   };
 
   return (
     <>
-      <h1 className="mt-20">This is Cart Page</h1>
-      <div>
-        {cartItem.length > 0 ? displayCartItems() : <h1>Cart is Empty</h1>}
-      </div>
-      <div className="bg-emerald-400 p-6 my-10 mx-20 rounded-md">
-        <h1>Your Cart:</h1>
+      <div></div>
+      <div className=" border h-full shadow-lg mt-28 mb-10 mx-6 rounded-md">
         <ul>
-          <li className="my-5 p-3 border rounded">
-            <div className="flex items-center mb-5">
-              <img
-                className="w-36 h-32 bg-cover "
-                src="https://m.media-amazon.com/images/I/61ZJlxRC2ZL._AC_UY900_.jpg"
-              />
-
-              <div className="grid grid-cols-2 mx-10 px-3 grid-rows-4">
-                <h2 className="row-start-1 font-bold">Product Name</h2>
-                <input
-                  className="row-start-4 w-12 h-8 rounded text-center bg-teal-100"
-                  type="number" name="qty" min="1" max="20" 
-                />
-                <p className="row-start-3 font-semibold">Price: $100</p>
-              </div>
-            </div>
-          </li>
-          
+          {cartItem.length > 0 ? displayCartItems() : <h1 className="text-center py-6 " >Cart is Empty</h1>}
         </ul>
       </div>
     </>
