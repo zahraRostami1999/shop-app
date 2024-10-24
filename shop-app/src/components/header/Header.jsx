790bf4faaab6b894272faffd1d8011f116e0402b
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-24 10:28:38 +0330

    edit Header component: add undefined and null condition for cartItem length that display on basket Icon

--- shop-app/src/components/header/Header.jsx
+++ shop-app/src/components/header/Header.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 
 function Header() {
   const { cartItem } = useSelector((state) => state.cart);
+
   return (
     <>
       <div className="flex justify-between text-slate-700 bg-red-100 p-4 text-center font-mono font-extrabold text-3xl fixed top-0 w-full">
@@ -15,13 +16,12 @@ function Header() {
           <div className="flex">
             <FontAwesomeIcon icon={faShoppingCart} size="lg" />
             <div className="bg-red-600 rounded-3xl text-xs font-thin h-5 px-2 py-0.5 inline -translate-x-3 -translate-y-2 cursor-pointer">
-              
               <div>
-                {cartItem.length > 0 ? (
-                  cartItem.length
-                ) : (
-                  0
-                )}
+                {cartItem.length === 0 ||
+                cartItem === undefined ||
+                cartItem === null
+                  ? 0
+                  : cartItem.length}
               </div>
             </div>
           </div>
