6e2858e098ae8b2f5405ab855b6a502c4e4dd3bc
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-25 21:43:18 +0330

    edit Header component: add Home and cloth icon for go home and product page

--- shop-app/src/components/header/Header.jsx
+++ shop-app/src/components/header/Header.jsx
@@ -1,5 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingCart,
+  faHome,
+  faTshirt,
+} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -10,22 +14,31 @@ function Header() {
     <>
       <div className="flex justify-between text-slate-700 bg-red-100 p-4 text-center font-mono font-extrabold text-3xl fixed top-0 w-full">
         <Link to="/">
-          <h1>WELLCOME TO YOUR SHOP</h1>
+          <h1>YOUR SHOP ❤️</h1>
         </Link>
-        <Link to="/cart">
-          <div className="flex">
-            <FontAwesomeIcon icon={faShoppingCart} size="lg" />
-            <div className="bg-red-600 rounded-3xl text-xs font-thin h-5 px-2 py-0.5 inline -translate-x-3 -translate-y-2 cursor-pointer">
-              <div>
-                {cartItem.length === 0 ||
-                cartItem === undefined ||
-                cartItem === null
-                  ? 0
-                  : cartItem.length}
+        <div className="w-48 flex justify-between">
+          <Link to="/">
+            <FontAwesomeIcon icon={faHome} size="x" />
+          </Link>
+          <Link>
+            <FontAwesomeIcon icon={faTshirt} size="x" />
+          </Link>
+
+          <Link to="/cart">
+            <div className="flex">
+              <FontAwesomeIcon icon={faShoppingCart} size="x" />
+              <div className="bg-red-500 rounded-3xl text-xs font-thin h-5 px-2 py-0.5 inline -translate-x-3 -translate-y-3 cursor-pointer">
+                <div className="font-mono font-medium">
+                  {cartItem.length === 0 ||
+                  cartItem === undefined ||
+                  cartItem === null
+                    ? 0
+                    : cartItem.length}
+                </div>
               </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
     </>
   );
