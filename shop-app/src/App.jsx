4ceca9ab8a7dbaf9a682f4fece015cdd0a2a9b6d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-25 21:41:26 +0330

    edit App.jsx: add Header component

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -3,11 +3,13 @@ import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import Page404 from "./pages/Page404";
 import ProductPage from "./pages/ProductPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route exact path="/" element={<HomePage />}></Route>
           <Route path="/cart" element={<CartPage />}></Route>
