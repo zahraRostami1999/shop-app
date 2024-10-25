a9f4d40f6d0f1c7822f3f753b57f13fb9622304c
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-25 12:01:09 +0330

    edit App.jsx: remove Header component above all pages

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
-import Header from "./components/header/Header";
 import Page404 from "./pages/Page404";
 import ProductPage from "./pages/ProductPage";
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route exact path="/" element={<HomePage />}></Route>
           <Route path="/cart" element={<CartPage />}></Route>
