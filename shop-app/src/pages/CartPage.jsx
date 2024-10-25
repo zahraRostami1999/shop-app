b35c14d204f985c9a0e5796fe5e6e56cebc6b513
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-25 18:45:33 +0330

    edit Cart Page: sort codes

--- shop-app/src/pages/CartPage.jsx
+++ shop-app/src/pages/CartPage.jsx
@@ -17,43 +17,23 @@ function CartPage() {
   }, []);
 
   const displayCartItems = () => {
-    return cartItem.map((item) => {
-      const matchingProduct = productItems.find(
-        (product) => item.id == product.id
-      );
-      if (matchingProduct) {
-        return (
-          <li
-            key={matchingProduct.id}
-            className=" rounded my-9 mx-6 flex items-center"
-          >
-            <img className="w-32 h-28 bg-cover " src={matchingProduct.image} />
-
-            <div className="grid grid-cols-2 grid-rows-2 ml-6">
-              <h2 className="row-start-1 font-bold">{matchingProduct.title}</h2>
-              <div className="row-start-4 flex my-2">
-                <button
-                  className="bg-green-300 px-2 rounded-full font-mono font-semibold text-center mx-2"
-                  onClick={() => dispatch(AddToCart(matchingProduct.id))}
-                >
-                  +
-                </button>
-                <p>{item.qty}</p>
-                <button
-                  className="bg-red-300 px-2  rounded-full font-mono font-semibold text-center mx-2"
-                  onClick={() => dispatch(removeFromCart(matchingProduct.id))}
-                >
-                  -
-                </button>
-              </div>
-              <p className="row-start-3 font-semibold">
-                Price: {matchingProduct.price} $
-              </p>
-            </div>
-          </li>
-        );
-      }
-    });
+    const matchingProduct = productItems.find(
+      (product) => item.id == product.id
+    );
+    if (matchingProduct) {
+      <li key={matchingProduct.id} className=" rounded my-9 mx-6 flex items-center">
+        <img className="w-32 h-28 bg-cover " src={matchingProduct.image} />
+        <div className="grid grid-cols-2 grid-rows-2 ml-6">
+          <h2 className="row-start-1 font-bold">{matchingProduct.title}</h2>
+          <div className="row-start-4 flex my-2">
+            <button className="bg-green-300 px-2 rounded-full font-mono font-semibold text-center mx-2" onClick={() => dispatch(AddToCart(matchingProduct.id))}>+</button>
+            <p>{item.qty}</p>
+            <button className="bg-red-300 px-2  rounded-full font-mono font-semibold text-center mx-2" onClick={() => dispatch(removeFromCart(matchingProduct.id))}>-</button>
+          </div>
+          <p className="row-start-3 font-semibold">Price: {matchingProduct.price} $ </p>
+        </div>
+      </li>;
+    }
   };
 
   return (
