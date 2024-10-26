5e891ce10348d2382199d10b615991531af38a3b
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-26 18:52:47 +0330

    edit vite.config.js: edit host IP

--- shop-app/vite.config.js
+++ shop-app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    host: '192.168.43.69',
+    host: '192.168.1.104',
     port: 4000,
   }
 })
