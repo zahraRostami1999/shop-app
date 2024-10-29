3066c6f9079b04237ca7f18cfbf56a5bd168c730
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-29 09:58:12 +0330

    edit Empty cart Page: add new cover and change description

--- shop-app/src/pages/EmptyCartPage.jsx
+++ shop-app/src/pages/EmptyCartPage.jsx
@@ -1,27 +1,28 @@
 import { Link } from "react-router-dom";
+import cover from '../assets/images/Empty.jpeg'
 
 function EmptyCartPage() {
   return (
     <>
-      <div className=" w-full h-screen flex justify-center items-center mt-8 font-sans">
-        <div className="bg-white w-2/5 h-5/6  top-28 left-96 rounded-md shadow-xl">
+      <div className=" bg-gray-50 w-full mt-8 font-Poppins">
+
           <img
-            src="https://i.pinimg.com/originals/81/c4/fc/81c4fc9a4c06cf57abf23606689f7426.jpg"
+            src={cover}
             alt="Empty menu illustration"
-            className="mx-auto mb-6 w-2/3 h-1/2 rounded-md"
+            className="mx-auto mt-10 mb-6 w-3/5 h-1/2 rounded-md"
           />
           <h1 className="text-zinc-800 text-center font-bold text-5xl">
             Your Cart is Empty!
           </h1>
-          <h2 className="text-zinc-700 text-2xl text-center my-6">
-            Go find the product you like.
+          <h2 className="text-zinc-700 text-2xl text-center my-2">
+          Let's see the new collection.
           </h2>
-          <Link to="/Products">
-            <button className="bg-red-400 text-zinc-900 text-xl text-center font-bold py-3 px-10 rounded-xl mx-40 my-10">
+          <Link className=" w-full flex justify-center" to="/Products">
+            <button className="bg-orange-500 text-zinc-900 text-xl text-center font-bold py-3 px-10 rounded-xl mt-6">
               Go Shopping 
             </button>
           </Link>
-        </div>
+
       </div>
     </>
   );
