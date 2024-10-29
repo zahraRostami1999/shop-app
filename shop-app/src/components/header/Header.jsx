5aaeb05726c0159d1f3f0f532cfb04f98e7f96d0
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-29 09:57:00 +0330

    edit Header component: change fonts to Poppins

--- shop-app/src/components/header/Header.jsx
+++ shop-app/src/components/header/Header.jsx
@@ -14,19 +14,19 @@ function Header() {
     <>
       <div className="flex justify-between text-gray-600  bg-blue-950 px-5 pt-4 pb-2 text-center font-mono font-extrabold text-3xl fixed top-0 w-full">
         <Link to="/">
-          <h1 className="font-great-vibes font-extrabold text-4xl text-orange-400">
+          <h1 className="font-great-vibes font-extrabold text-4xl text-orange-500">
           SportyFit
           </h1>
         </Link>
         <div className="w-1/5  flex justify-between text-white">
           <div className="flex w-3/4  justify-around ">
             <Link to="/">
-              <p className="font-sans font-bold text-xl transition duration-600 delay-75 ease-in-out transform  hover:hover:border-b-2 hover:border-white">
+              <p className="font-Poppins font-bold text-xl transition duration-600 delay-75 ease-in-out transform  hover:hover:border-b-2 hover:border-white">
                 Home
               </p>
             </Link>
             <Link to='/Products'>
-              <p className="font-sans font-bold text-xl transition duration-600 delay-75 ease-in-out transform  hover:hover:border-b-2 hover:border-white">
+              <p className="font-Poppins font-bold text-xl transition duration-600 delay-75 ease-in-out transform  hover:hover:border-b-2 hover:border-white">
                 Shop
               </p>
             </Link>
@@ -35,7 +35,7 @@ function Header() {
           <Link to="/cart">
             <div className="flex text-white ">
               <FontAwesomeIcon icon={faShoppingCart} size="x" />
-              <div className="bg-orange-400 rounded-3xl text-xs font-normal h-5 px-2 py-0.5 inline -translate-x-3 -translate-y-3 cursor-pointer">
+              <div className="bg-orange-500 rounded-3xl text-xs font-normal h-5 px-2 py-0.5 inline -translate-x-3 -translate-y-3 cursor-pointer">
                 <div className="font-mono font-medium">
                   {cartItem.length === 0 ||
                   cartItem === undefined ||
