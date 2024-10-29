f65e0b28978f2bb7f47d26fc523b607d85b7cbd1
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-29 10:36:47 +0330

    edit Product Page: change product image size to full

--- shop-app/src/pages/ProductPage.jsx
+++ shop-app/src/pages/ProductPage.jsx
@@ -23,25 +23,35 @@ function ProductPage() {
 
   return (
     <>
-      <div className="mt-24 border shadow-xl mx-auto py-3 w-5/12">
-        <h1 className="text-2xl font-bold text-center py-3">
-          {productDetails.title}
-        </h1>
-        <img
-          src={productDetails.image}
-          alt={productDetails.title}
-          className=" w-1/2 h-96 mx-auto my-3"
-        />
-        <h2 className="text-lg font-bold text-center">
-          {productDetails.price} $
-        </h2>
-        <h3 className="text-base px-10">{productDetails.description}</h3>
-        <div className="flex justify-center items-center">
-          <Link to="/cart">
-            <button onClick={() => handleAddToBasket(Params.id)} className="bg-yellow-500 px-16 py-2 rounded-md my-5 font-semibold">
-              Add to Basket
-            </button>
-          </Link>
+      <div className=" h-3/4 mt-24 border shadow-xl mx-auto py-3 flex ">
+        <div className=" w-3/4">
+          <img
+            src={productDetails.image}
+            alt={productDetails.title}
+            className=" w-full  mx-auto my-3"
+          />
+        </div>
+        <div className="mx-5 ">
+          <h1 className="text-3xl font-bold my-5 py-3">
+            {productDetails.title}
+          </h1>
+          <h3 className=" h-1/2 text-xl mb-16">{productDetails.description}</h3>
+          <div className="w-1/2  flex justify-between items-center">
+            <h2 className="text-lg font-bold ">
+            €{productDetails.price}
+            </h2>
+
+            <div className="flex items-center">
+              <Link to="/cart">
+                <button
+                  onClick={() => handleAddToBasket(Params.id)}
+                  className="bg-orange-500 px-16 py-2 rounded-md my-5 font-semibold"
+                >
+                  Add to Basket
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
