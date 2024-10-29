e19f5ab3b2a6c4bee7055bc7ed7b906fd632273e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-29 08:46:00 +0330

    edit Home Page: add new cover

--- shop-app/src/pages/HomePage.jsx
+++ shop-app/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
-import cover from "../assets/images/HomePage5.jpeg";
+import cover from "../assets/images/HomePage.jpeg";
 
 function HomePage() {
   return (
     <>
-      <div className="bg-white w-full h-screen flex justify-between pt-32 px-10 ">
+      <div className="bg-gray-100 w-full h-screen flex justify-between pt-32 px-10 ">
         <div className="pr-16 font-sans ">
           <h1 className="my-10 mb-20 text-2xl font-bold text-zinc-700">Welcome to Moonlit Couture🌜</h1>
           <p className="my-8 text-xl font-semibold text-gray-700">
