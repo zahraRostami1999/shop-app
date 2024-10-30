9774a75448cd2b87acd2be12e60b46d4469f2aed
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-30 19:39:31 +0330

    edit Home Page: add btn to link the store

--- shop-app/src/pages/HomePage.jsx
+++ shop-app/src/pages/HomePage.jsx
@@ -4,11 +4,11 @@ function HomePage() {
   return (
     <>
       <div className="bg-gray-100 w-full h-screen flex justify-between pt-32 px-10 ">
-        <div className="pr-16 font-Poppins ">
-          <h1 className="mt-20 mb-10 text-2xl font-bold text-slate-950-900">
+        <div className=" font-Poppins pl-10">
+          <h1 className="mt-24 mb-10 text-3xl font-bold text-gray-800">
             Welcome to SportyFit sports store!
           </h1>
-          <p className="my-12 text-xl font-semibold text-gray-700">
+          <p className="my-14 text-xl font-semibold text-gray-700">
             <p>
               Find the best in sportswear and fashion here! From comfortable
               t-shirts and functional pants to yoga and running apparel.
@@ -17,8 +17,10 @@ function HomePage() {
               With brands like Nike, Adidas, Puma, and Reebok, you can always
               look your best in style and performance
             </p>
-            <h3 className="my-12">Buy now and enjoy the sport!</h3>
           </p>
+          <div className="w-full my-16">
+            <button className="bg-orange-500 w-5/6 py-3 rounded-lg font-semibold text-lg shadow-lg shadow-slate-300 text-white hover:scale-105 transition duration-300">Buy now and enjoy the sport</button>
+          </div>
         </div>
 
         <img className="bg-cover w-4/6 h-5/6 " src={cover} />
