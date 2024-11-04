432ca15c28137ef00c021252964a91c5257cde0d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-04 22:15:12 +0330

    edit Store: add IsLogin Slice

--- shop-app/src/redux/Store.jsx
+++ shop-app/src/redux/Store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { CartSlice } from "./CartSlice";
+import { IsLogin } from "./IsLogin";
 
 export const Store = configureStore({
   reducer: {
     cart: CartSlice.reducer,
+    isLogin: IsLogin.reducer,
   },
 });
