8a0cef06f4ad0d64b9da0c3a7d15ae313f528607
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-05 20:00:25 +0330

    edit IsLogin slice: add logoutUser reducer

--- shop-app/src/redux/IsLogin.jsx
+++ shop-app/src/redux/IsLogin.jsx
@@ -11,13 +11,15 @@ export const IsLogin = createSlice({
   },
   reducers: {
     checkUser: (state, actions) => {
-
       let username = actions.payload.username;
       let password = actions.payload.password;
       state.isLoggedIn = state.user.userName === username && state.user.passWord === password;
     },
+    logoutUser: (state) => {
+      state.isLoggedIn = false;
+    },
   },
 });
 
-export const { checkUser } = IsLogin.actions;
+export const { checkUser, logoutUser } = IsLogin.actions;
 export default IsLogin.reducer;
