899dd252cca6a95bd2e06c12dc3eff80d41a78a4
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-05 16:45:52 +0330

    edit isLogin Slice: add user to initail state

--- shop-app/src/redux/IsLogin.jsx
+++ shop-app/src/redux/IsLogin.jsx
@@ -3,9 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 export const IsLogin = createSlice({
     name: "IsLogin",
     initialState: {
-        isLoggedIn: false
+        isLoggedIn: false,
+        user: {
+            'zahra': 12345,
+        }
     },
-    reducers:{}
+    reducers:{
+        
+    }
 })
 
 export default IsLogin.reducer;
