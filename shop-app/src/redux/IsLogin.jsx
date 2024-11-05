199d42bdefdc079daca617b2516f1e1d4a1263f3
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-05 18:13:45 +0330

    edit IsLogin Slice: add checkUser reducer

--- shop-app/src/redux/IsLogin.jsx
+++ shop-app/src/redux/IsLogin.jsx
@@ -1,16 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const IsLogin = createSlice({
-    name: "IsLogin",
-    initialState: {
-        isLoggedIn: false,
-        user: {
-            'zahra': 12345,
-        }
+  name: "IsLogin",
+  initialState: {
+    isLoggedIn: false,
+    user: {
+      userName: "zahra",
+      passWord: "12345",
     },
-    reducers:{
-        
-    }
-})
+  },
+  reducers: {
+    checkUser: (state, actions) => {
+      let username = actions.payload.username;
+      let password = actions.payload.password;
 
+      if (
+        state.user.userName === username &&
+        state.user.passWord === password
+      ) {
+        state.isLoggedIn = true;
+      } else {
+        state.isLoggedIn = false;
+      }
+    },
+  },
+});
+
+export const { checkUser } = IsLogin.actions;
 export default IsLogin.reducer;
