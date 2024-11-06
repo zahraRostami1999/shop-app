714685c9ae3d9f0e66bca44fe339a0d91f2a844a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-06 19:46:06 +0330

    edit Home Page: add say hello function to say hello after logged In

--- shop-app/src/pages/HomePage.jsx
+++ shop-app/src/pages/HomePage.jsx
@@ -1,12 +1,29 @@
 import cover from "../assets/images/HomePage.jpeg";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function HomePage() {
+  const { isLoggedIn } = useSelector((state) => state.isLogin);
+  const { user } = useSelector((state) => state.isLogin);
+  const userName = user.userName;
+
+  const sayHello = () => {
+    if (isLoggedIn) {
+      const greeting = userName ? `Hello ${userName}` : "Hello";
+      return (
+        <div className="pt-20 text-3xl font-bold bg-gray-100 px-20 ">
+          <h1>{greeting}</h1>
+        </div>
+      );
+    }
+  };
+
   return (
     <>
-      <div className="bg-gray-100 w-full h-screen flex justify-between pt-32 px-10 ">
+      {sayHello()}
+      <div className="bg-gray-100 w-full h-screen flex justify-between pt-24 px-10 ">
         <div className=" font-Poppins pl-10">
-          <h1 className="mt-24 mb-10 text-3xl font-bold text-gray-800">
+          <h1 className="mt-20 mb-10 text-3xl font-bold text-gray-800">
             Welcome to SportyFit sports store!
           </h1>
           <p className="my-14 text-xl font-semibold text-gray-700">
