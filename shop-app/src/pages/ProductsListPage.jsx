90f4998e223e46f0b115471fef785c9a6bec9bbb
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-06 10:44:16 +0330

    edit ProductsListPage: add brands filter

--- shop-app/src/pages/ProductsListPage.jsx
+++ shop-app/src/pages/ProductsListPage.jsx
@@ -3,6 +3,14 @@ import Products from "../components/products/Products";
 function ProductsListPage(){
   return (
     <>
+    <div className="mt-20 w-full flex justify-center">
+      <ul className="list-none flex w-1/2 py-5 text-xl font-Poppins font-medium justify-around">
+        <li className="cursor-pointer">Adidas</li>
+        <li className="cursor-pointer">Nike</li>
+        <li className="cursor-pointer">Puma</li>
+        <li className="cursor-pointer">Reebok</li>
+      </ul>
+    </div>
       <Products />
     </>
   );
