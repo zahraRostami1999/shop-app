9da5fc2246a8e7012c2b582a4522d621769ce87a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-06 10:49:26 +0330

    edit ProductsListPage: add FilterBrands component

--- shop-app/src/pages/ProductsListPage.jsx
+++ shop-app/src/pages/ProductsListPage.jsx
@@ -1,19 +1,15 @@
 import Products from "../components/products/Products";
+import FilterBrands from "../components/filterBrands/FilterBrands";
 
-function ProductsListPage(){
+function ProductsListPage() {
   return (
     <>
-    <div className="mt-20 w-full flex justify-center">
-      <ul className="list-none flex w-1/2 py-5 text-xl font-Poppins font-medium justify-around">
-        <li className="cursor-pointer">Adidas</li>
-        <li className="cursor-pointer">Nike</li>
-        <li className="cursor-pointer">Puma</li>
-        <li className="cursor-pointer">Reebok</li>
-      </ul>
-    </div>
+      <div className="mt-20 w-full flex justify-center">
+        <FilterBrands />
+      </div>
       <Products />
     </>
   );
-};
+}
 
 export default ProductsListPage;
