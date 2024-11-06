7e9fdabbc7c664cd3f4d15efd83bf363260d6c93
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-06 12:09:08 +0330

    edit ProductsListPage: remove import FilterBrands

--- shop-app/src/pages/ProductsListPage.jsx
+++ shop-app/src/pages/ProductsListPage.jsx
@@ -1,12 +1,8 @@
 import Products from "../components/products/Products";
-import FilterBrands from "../components/filterBrands/FilterBrands";
 
 function ProductsListPage() {
   return (
     <>
-      <div className="mt-20 w-full flex justify-center">
-        <FilterBrands />
-      </div>
       <Products />
     </>
   );
