9a5002ec39aa7d81af75ff82c6f76f7fcaf952bd
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-06 11:36:46 +0330

    edit Products component: add buttons for filter products

--- shop-app/src/components/products/Products.jsx
+++ shop-app/src/components/products/Products.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import Card from "../card/Card";
-import axios from "axios";
 import { getProducts } from "../../services/api";
-import { Link } from "react-router-dom";
 
 function Products() {
   const [productData, setProductData] = useState([]);
+  const [fiteredData, setFiteredData] = useState([]);
 
   useEffect(() => {
     getProducts()
       .then((result) => {
         setProductData(result);
+        setFiteredData(result);
       })
       .catch((error) => {
         console.log(error);
@@ -18,6 +18,12 @@ function Products() {
   }, []);
   return (
     <>
+      <div>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
       <div className="mt-5 p-10 grid grid-cols-4 ">
         {productData.map((product) => (
           <Card key={product.id} {...product} />
