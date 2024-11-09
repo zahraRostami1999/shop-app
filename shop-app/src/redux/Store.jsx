462c77706e017641d655f28fd0cd6464b0ec8ef7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-09 17:00:24 +0330

    edit Store.jsx: remove IsLogin Slice

--- shop-app/src/redux/Store.jsx
+++ shop-app/src/redux/Store.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { CartSlice } from "./CartSlice";
-import { IsLogin } from "./IsLogin";
 
 export const Store = configureStore({
   reducer: {
     cart: CartSlice.reducer,
-    isLogin: IsLogin.reducer,
   },
 });
