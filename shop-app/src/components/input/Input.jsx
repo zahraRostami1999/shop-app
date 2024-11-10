20177f07d7f922cb73a5953102c3b82ead190d8a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-10 12:13:19 +0330

    edit Input component: increase my

--- shop-app/src/components/input/Input.jsx
+++ shop-app/src/components/input/Input.jsx
@@ -2,10 +2,10 @@
 function Input(props){
 
 return(
-    <div className="my-5 w-full  flex justify-between items-center pr-5">
+    <div className="my-8 w-full  flex justify-between items-center pr-5">
     <label className="w-1/3">{props.label} </label>
     <input
-      className="w-3/4 h-8 rounded-lg border-2 border-zinc-200 px-3"
+      className="w-2/3 h-9 rounded-lg border-2 border-zinc-200 px-3 focus:border-zinc-400"
       type="text"
     />
   </div>
