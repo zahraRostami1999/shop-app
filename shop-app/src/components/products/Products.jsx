c70df42333654c1e14d6a94fcc6a2c9d7d245bbf
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-06 22:57:58 +0330

    edit Products: make responsive

--- shop-app/src/components/products/Products.jsx
+++ shop-app/src/components/products/Products.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function Products() {
   const [productData, setProductData] = useState([]);
-  const [filter, setFilter] = useState("All"); // Use a state variable for the filter
+  const [filter, setFilter] = useState("All");
 
   useEffect(() => {
     getProducts()
@@ -27,45 +27,46 @@ function Products() {
   };
 
   const scrollDown = () => {
-    window.scrollBy({ top: window.innerHeight-50, behavior: "smooth" });
+    window.scrollBy({ top: window.innerHeight -80, behavior: "smooth" });
   };
 
   return (
     <>
-      <div className="mt-24 flex justify-around px-96 text-lg font-Poppins font-medium ">
+      <div className="mt-24 flex flex-wrap justify-center gap-2 px-4 lg:px-24 md:px-12 sm:px-6 text-xs lg:text-lg md:text-lg font-Poppins font-medium">
         <button
           onClick={() => handleFilterProducts("All")}
-          className="border-2 border-orange-400 w-24 h-10 rounded-md hover:bg-orange-400 transition-all duration-300"
+          className="border-2 border-orange-400 px-2 py-2 rounded-md hover:bg-orange-400 transition-all duration-300"
         >
           All
         </button>
         <button
           onClick={() => handleFilterProducts("Adidas")}
-          className="border-2 border-orange-400 w-24 h-10 rounded-md hover:bg-orange-400 transition-all duration-300"
+          className="border-2 border-orange-400 px-2 py-2 rounded-md hover:bg-orange-400 transition-all duration-300"
         >
           Adidas
         </button>
         <button
           onClick={() => handleFilterProducts("Nike")}
-          className="border-2 border-orange-400 w-24 h-10 rounded-md hover:bg-orange-400 transition-all duration-300"
+          className="border-2 border-orange-400 px-2 py-2 rounded-md hover:bg-orange-400 transition-all duration-300"
         >
           Nike
         </button>
         <button
           onClick={() => handleFilterProducts("Puma")}
-          className="border-2 border-orange-400 w-24 h-10 rounded-md hover:bg-orange-400 transition-all duration-300"
+          className="border-2 border-orange-400 px-2 py-2 rounded-md hover:bg-orange-400 transition-all duration-300"
         >
           Puma
         </button>
         <button
           onClick={() => handleFilterProducts("Reebok")}
-          className="border-2 border-orange-400 w-24 h-10 rounded-md hover:bg-orange-400 transition-all duration-300"
+          className="border-2 border-orange-400 px-2 py-2 rounded-md hover:bg-orange-400 transition-all duration-300"
         >
           Reebok
         </button>
       </div>
-      <div className="mt-5 p-10 grid grid-cols-4 ">
-        <div className="fixed bottom-2 right-2 w-10">
+
+      <div className="mt-5 lg:p-10 md:p-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
+        <div className="fixed bottom-16 right-2 w-10">
           <button
             onClick={scrollUp}
             className="text-zinc-700  text-center border-zinc-500 px-3 py-2  rounded-full shadow-xl shadow-zinc-900 hover:scale-95 transition duration-300 text-xl hover:text-zinc-800"
