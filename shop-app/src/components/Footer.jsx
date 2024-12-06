1517c49f6847fad2bc5cf6f7b4c3edca8346d90e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-06 22:58:18 +0330

    edit Footer: add Footer

---
+++ shop-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+            <div className="bg-blue-950 w-full h-14 flex justify-center items-center font-semibold">
+                <h2 className="text-white lg:text-base md:text-base text-sm">2024@Built with Zahra Rostami, <span className="text-orange-500">Your name place*</span></h2>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
