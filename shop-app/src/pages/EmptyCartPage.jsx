e6f9aefdf319a38f1724964423b25a49c59624d7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-06 22:56:50 +0330

    edit EmptyCartPage: make responsive

--- shop-app/src/pages/EmptyCartPage.jsx
+++ shop-app/src/pages/EmptyCartPage.jsx
@@ -4,21 +4,21 @@ import cover from '../assets/images/Empty.jpeg'
 function EmptyCartPage() {
   return (
     <>
-      <div className=" bg-gray-50 w-full mt-8 font-Poppins">
+      <div className="w-full mt-10 font-Poppins">
 
           <img
             src={cover}
             alt="Empty menu illustration"
             className="mx-auto mt-10 mb-6 w-3/5 h-1/2 rounded-md"
           />
-          <h1 className="text-zinc-800 text-center font-bold text-5xl">
+          <h1 className="text-zinc-800 text-center font-bold lg:text-5xl lg:text-5xl text-3xl">
             Your Cart is Empty!
           </h1>
-          <h2 className="text-zinc-700 text-2xl text-center my-2">
+          <h2 className="text-zinc-700 text-lg lg:text-2xl md:text-2xl text-center my-2">
           Let's see the new collection.
           </h2>
           <Link className=" w-full flex justify-center" to="/Products">
-            <button className="bg-orange-500 hover:scale-105 transition duration-100 ease-in-out text-zinc-900 text-xl text-center font-bold py-3 px-10 rounded-xl mt-6">
+            <button className="bg-orange-500 hover:scale-105 transition duration-100 ease-in-out text-zinc-900 lg:text-xl md:lg:text-xl text-lg text-center font-bold py-3 px-10 rounded-xl mt-6">
               Let's Go 
             </button>
           </Link>
