18f8f2f0e692db2c6b85b3a2a8cb355f13ab379d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-07 14:42:26 +0330

    fix local data

--- shop-app/vite.config.js
+++ shop-app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    // host: '192.168.1.104',
-    port: 4000,
+    host: '127.0.0.1',
+    port: 40000,
   }
 })
