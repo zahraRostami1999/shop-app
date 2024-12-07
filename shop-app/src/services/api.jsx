fc915bc3ff274bcf844e4c76dc688b8dffa72ae1
zahraRostami1999 <zahrarostami137878@gmail.com>
2024-12-07 14:11:20 +0330

    edit api: remove select projectPhase

--- shop-app/src/services/api.jsx
+++ shop-app/src/services/api.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const projectPhase = process.env.NODE_ENV;
-const domain = (projectPhase == 'development') ? 'zahrarostami.away.pk' : window.location.hostname;
+const domain = window.location.hostname;
 
 const client = axios.create({
   baseURL: `http://${domain}:4500`,
@@ -14,6 +13,5 @@ export async function getProducts() {
 
 export async function getProductDetails(id) {
   const { data } = await client(`/Products/${id}`);
-
   return data;
 }
