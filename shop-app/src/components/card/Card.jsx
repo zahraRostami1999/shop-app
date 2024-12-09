d48caf420bf4abe943c7f0ebde0b2ec4f36f16e8
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-09 21:06:09 +0330

    edit Card: set link for inside content of cards

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { AddToCart } from "../../redux/CartSlice";
 
-function Card({ id, title, price, description, image }) {
+function Card({ id, title, price, image }) {
   const dispatch = useDispatch();
 
   const handleAddToBasket = (id) => {
@@ -11,19 +11,21 @@ function Card({ id, title, price, description, image }) {
 
   return (
     <>
-      <Link to={`/Product/${id}`}>
-        <div className="shadow-gray-400 shadow-lg pb-5 border-zinc-300 border-2 lg:mx-7 md:mx-7 mx-2 rounded-2xl mb-40 mt-10 bg-gradient-to-tr from-zinc-300 to-stone-100 hover:border-zinc-300 hover:shadow-gray-900 hover:shadow-2xl transition duration-500 ease-in-out">
+
+      <div className="shadow-gray-400 shadow-lg pb-16 border-zinc-300 border-2 lg:mx-7 md:mx-7 mx-2 rounded-2xl mb-10 mt-10 bg-gradient-to-tr from-zinc-300 to-stone-100 hover:border-zinc-300 hover:shadow-gray-900 hover:shadow-2xl transition duration-500 ease-in-out">
+        <Link to={`/Product/${id}`}>
           <img className="w-full h-1/2 rounded-t-2xl" src={image} />
           <div className="px-1 lg:px-3 md:px-3">
-            <h2 className="h-10 text-left text-zinc-800 text-xs lg:text-base md:text-base font-Poppins pt-3 font-bold">
+            <h2 className="h-16 text-left text-zinc-800 text-xs lg:text-base md:text-base font-Poppins pt-3 font-bold">
               {title}
             </h2>
             <p className="font-bold flex text-zinc-900 justify-between items-center">
               <span>€{price}</span>
             </p>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
+
     </>
   );
 }
