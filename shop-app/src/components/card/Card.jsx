782cf98475bcacd6ed94680052590a12b2629c04
zahraRostami1999 <zahrarostami137878@gmail.com>
2024-12-09 22:45:25 +0330

    delete ex images

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -1,9 +1,5 @@
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-
 function Card({ id, title, price, image }) {
-  const dispatch = useDispatch();
-
   return (
     <>
       <div className="shadow-gray-400 shadow-lg pb-16 border-zinc-300 border-2 lg:mx-7 md:mx-7 mx-2 rounded-2xl mb-10 mt-10 bg-gradient-to-tr from-zinc-300 to-stone-100 hover:border-zinc-300 hover:shadow-gray-900 hover:shadow-2xl transition duration-500 ease-in-out">
