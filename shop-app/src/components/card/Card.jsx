dde364a7ad621b651b2a69e18f3c54285c4d642f
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-09 21:06:59 +0330

    edit Card: remove extra codes

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -1,17 +1,11 @@
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { AddToCart } from "../../redux/CartSlice";
 
 function Card({ id, title, price, image }) {
   const dispatch = useDispatch();
 
-  const handleAddToBasket = (id) => {
-    dispatch(AddToCart(id));
-  };
-
   return (
     <>
-
       <div className="shadow-gray-400 shadow-lg pb-16 border-zinc-300 border-2 lg:mx-7 md:mx-7 mx-2 rounded-2xl mb-10 mt-10 bg-gradient-to-tr from-zinc-300 to-stone-100 hover:border-zinc-300 hover:shadow-gray-900 hover:shadow-2xl transition duration-500 ease-in-out">
         <Link to={`/Product/${id}`}>
           <img className="w-full h-1/2 rounded-t-2xl" src={image} />
@@ -25,7 +19,6 @@ function Card({ id, title, price, image }) {
           </div>
         </Link>
       </div>
-
     </>
   );
 }
