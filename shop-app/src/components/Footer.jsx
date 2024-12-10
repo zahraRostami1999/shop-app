8353df8aa8fd8674f007972a99da046e0b0903f2
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-10 20:05:43 +0330

    edit Footer: add names

--- shop-app/src/components/Footer.jsx
+++ shop-app/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <>
             <div className="bg-blue-950 w-full h-14 flex justify-center items-center font-semibold">
-                <h2 className="text-white lg:text-base md:text-base text-sm">2024@Built with Zahra Rostami, <span className="text-orange-500">Your name place*</span></h2>
+                <h2 className="text-white lg:text-base md:text-base text-xs text-center">2024@Built with Zahra Rostami, Sadra Yavarzadeh, Hossein Mohammadzadeh, <span className="text-orange-500">Your name place*</span></h2>
             </div>
         </>
     )
