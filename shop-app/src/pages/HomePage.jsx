d55fbfb1d72f4b1ca36537beaf1d30af1202fd61
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-10 17:37:37 +0330

    edit HomePage: set content center and change carousel location under welcome

--- shop-app/src/pages/HomePage.jsx
+++ shop-app/src/pages/HomePage.jsx
@@ -5,13 +5,10 @@ function HomePage() {
   return (
     <>
       <div className="min-h-screen w-full flex flex-wrap justify-between lg:pt-20 md:pt-24 lg:px-10 px-2">
-        <div className="w-full">
-          <Carousel />
-        </div>
-        <div className="lg:h-screen md:h-full h-full w-full">
+        <div className=" h-full w-full">
           <img className="bg-cover lg:w-2/3 md:w-5/6 lg:h-5/6 md:h-5/6 w-11/12 mx-auto" src={cover} />
         </div>
-        <div className=" font-Poppins lg:pl-10 pl-0 pt-2">
+        <div className="w-full font-Poppins lg:pl-10 pl-0 pt-10 text-center">
           <h1 className="lg:mb-5 mb-2 text-lg font-semibold lg:text-3xl md:text-3xl lg:font-bold md:font-bold text-gray-800">
             Welcome to SportyFit sports store!
           </h1>
@@ -25,9 +22,12 @@ function HomePage() {
               look your best in style and performance.
             </p>
           </p>
+          <div className="w-full">
+            <Carousel />
+          </div>
           <Link to="/Products">
             <div className="w-full lg:w-full md-2/3 lg:my-16 md:my-10 my-10 flex justify-center">
-              <button className="bg-orange-500 w-2/3 py-3 rounded-lg lg:font-semibold md:font-semibold lg:text-lg md:text-lg text-xs shadow-lg shadow-slate-300 text-white hover:scale-105 transition duration-300">
+              <button className="bg-orange-500 w-2/5 py-3 rounded-lg lg:font-semibold md:font-semibold lg:text-lg md:text-lg text-xs shadow-lg shadow-slate-400 text-white hover:scale-105 transition duration-300">
                 Buy now and enjoy the sport
               </button>
             </div>
