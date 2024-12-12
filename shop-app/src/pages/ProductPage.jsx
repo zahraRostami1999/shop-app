2721dc55c5899de8a830e82a7c21fb45e7f0c5f5
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-12 13:17:18 +0330

    edit ProductPage: add back to store btn

--- shop-app/src/pages/ProductPage.jsx
+++ shop-app/src/pages/ProductPage.jsx
@@ -4,6 +4,7 @@ import { getProductDetails } from "../services/api";
 import { useDispatch } from "react-redux";
 import { AddToCart } from "../redux/CartSlice";
 import { ToastContainer, toast } from 'react-toastify';
+import { Link } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 
 function ProductPage() {
@@ -26,7 +27,7 @@ function ProductPage() {
     <>
       <div className="w-full min-h-screen">
         <div className="h-full mx-auto mt-20 py-5 flex flex-col lg:flex-row justify-center lg:justify-around">
-          <div className="lg:w-2/5 md:w-2/5 w-2/5 h-1/3 lg:h-auto mx-auto">
+          <div className="lg:w-1/3 md:w-2/5 w-3/5 h-1/3 lg:h-auto mx-auto">
             <img
               src={productDetails.image}
               alt={productDetails.title}
@@ -53,6 +54,15 @@ function ProductPage() {
                 >
                   Add to Basket
                 </button>
+              </div>
+              <div>
+                <Link to="/Products">
+                  <button
+                    className="text-sm lg:text-base md:text-base bg-zinc-400 lg:px-16 md:px-16 px-3 py-2 rounded-md my-0 font-semibold hover:scale-110 transition duration-500 ease-in-out"
+                  >
+                    Back to Store
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
