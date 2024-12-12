7d3aed1f9d8fd4bac36a7d31bb86c259b87048d9
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-12 13:16:12 +0330

    edit Card

--- shop-app/src/components/card/Card.jsx
+++ shop-app/src/components/card/Card.jsx
@@ -2,19 +2,27 @@ import { Link } from "react-router-dom";
 function Card({ id, title, price, image }) {
   return (
     <>
-      <div className="shadow-gray-400 shadow-lg border-zinc-300 border-2 lg:mx-7 md:mx-7 mt-10 mx-2 rounded-2xl mb-20 mt-10 bg-gradient-to-tr from-zinc-300 to-stone-100 hover:border-zinc-300 hover:shadow-gray-900 hover:shadow-2xl transition duration-500 ease-in-out">
-        <Link to={`/${id}`}>
-          <img className="w-full h-60  rounded-t-2xl" src={image} />
-          <div className="px-1 lg:px-3 md:px-3">
-            <h2 className="h-16 text-left text-zinc-800 text-xs lg:text-base md:text-base font-Poppins pt-3 font-bold">
-              {title}
-            </h2>
-            <p className="font-bold flex text-zinc-900 justify-between items-center pb-10">
-              <span>€{price}</span>
-            </p>
-          </div>
-        </Link>
-      </div>
+<div
+              
+              className="bg-neutral-100 px-1 overflow-hidden shadow-gray-400 shadow-md border-zinc-200 border-2 lg:mx-5 mt-10 mx-3 rounded-2xl hover:border-zinc-300 hover:shadow-gray-900 hover:shadow-2xl transition duration-500 ease-in-out">
+              <Link to={`/${id}`}>
+                <div className="relative mx-auto lg:w-64 md:w-56 lg:h-52 md:h-52 sm:h-44 sm:w-5/6 h-32 w-1/2">
+                  <img
+                    className="inset-0 h-full w-full rounded-t-2xl "
+                    src={image}
+                    alt={title}
+                  />
+                </div>
+                <div className="px-5 lg:px-3 md:px-3 break-words whitespace-normal sm:text-center">
+                  <h2 className="lg:h-16 flex flex-wrap md:h-16  text-zinc-800 text-sm lg:text-base md:text-base font-Poppins pt-3 font-bold">
+                    {title}
+                  </h2>
+                  <p className="font-bold flex text-zinc-900 justify-between items-center pb-5 lg:pb-10 md:pb-10">
+                    <span className="text-green-700">€{price}</span>
+                  </p>
+                </div>
+              </Link>
+            </div>
     </>
   );
 }
