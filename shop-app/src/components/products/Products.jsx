d3916b3f46ead5de0508480869897337f70263cf
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-12 13:16:55 +0330

    edit Products: add back top btn

--- shop-app/src/components/products/Products.jsx
+++ shop-app/src/components/products/Products.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useMemo } from "react";
 import Card from "../card/Card";
 import { getProducts } from "../../services/api";
 import React from "react";
+import BackTop from '../BackTop.jsx'
 
 function Products() {
   const [productData, setProductData] = useState([]);
@@ -25,15 +26,18 @@ function Products() {
 
   return (
     <>
-      <div className="mt-24 flex flex-wrap justify-center gap-2 px-4 lg:px-24 md:px-12 sm:px-6 text-xs lg:text-lg md:text-lg font-Poppins font-medium">
+      <div className="mt-20 flex flex-wrap justify-center gap-2 px-4 lg:px-24 md:px-12 sm:px-6 text-xs lg:text-base md:text-base font-Poppins font-medium">
         {filterBtn.map((btn) => {
           return <button className="border-2 border-orange-400 px-2 py-2 rounded-md hover:bg-orange-400 transition-all duration-300" key={btn} onClick={() => handleFilterProducts(btn)}>{btn}</button>
         })}
       </div>
-      <div className="min-h-screen mt-5 lg:p-10 md:p-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
+      <div className="min-h-screen xl:mx-10 lg:mx-8 md:mx-6 mx-3 mb-20 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 gap-3">
         {filteredProducts.map((product) => (
           <Card key={product.id} {...product} />
         ))}
+      </div>
+      <div className="">
+        <BackTop />
       </div>
     </>
   );
