0acb02f30569559899954fdc45a2cd5dd04535e1
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-13 13:30:30 +0330

    edit api: add new API

--- shop-app/src/services/api.jsx
+++ shop-app/src/services/api.jsx
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-const domain = "api.zahra-rostami.ir";
-
 const client = axios.create({
-  baseURL: `https://${domain}`,
+  baseURL: "https://fakestoreapi.com",
 });
 
 export async function getProducts() {
-  const { data } = await client("/Products");
+  const { data } = await client("/products");
   return data;
 }
 
 export async function getProductDetails(id) {
-  const { data } = await client(`/Products/${id}`);
+  const { data } = await client(`/products/${id}`);
   return data;
 }
