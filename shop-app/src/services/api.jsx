d477a776a1e72b1604c9e914603a983933b1ca79
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-14 00:02:06 +0330

    edit api: add get all users API

--- shop-app/src/services/api.jsx
+++ shop-app/src/services/api.jsx
@@ -13,3 +13,15 @@ export async function getProductDetails(id) {
   const { data } = await client(`/products/${id}`);
   return data;
 }
+
+export const verifyUser = async (info) => {
+  try {
+    const { data } = await axios.get('https://fakestoreapi.com/users');
+      console.log(data);
+      const ckeckUser = data.find(user => user.username === info.username && user.password === info.password);
+      return ckeckUser;
+  } catch {
+      console.error('Error verifying user:', error);
+      return false;
+  }
+};
