367419b3e7954af8faaf310fa5893a434b84e9d9
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-14 13:43:57 +0330

    edit Store: add UserSlice

--- shop-app/src/redux/Store.jsx
+++ shop-app/src/redux/Store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { CartSlice } from "./CartSlice";
+import { UserSlice } from "./UserSlice";
 
 export const Store = configureStore({
   reducer: {
     cart: CartSlice.reducer,
+    user: UserSlice.reducer,
   },
 });
