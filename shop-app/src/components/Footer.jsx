1360e4e77a67353b33ee0b8c30463c5cd2017890
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-14 20:38:52 +0330

    edit Footer: add new names and change bg color to neutral 950

--- shop-app/src/components/Footer.jsx
+++ shop-app/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <>
-            <div className="bg-blue-950 w-full px-5 h-20 flex justify-center items-center font-semibold pb-0">
-                <h2 className="text-white lg:text-base md:text-sm text-xs text-center">Developed by Zahra Rostami, Sadra Yavarzadeh, Hossein Mohammadzadeh, Mahdi Hashemi, Sahar Fallahi, Ahmadreza Navaie, <span className="text-orange-500">Your name place*</span></h2>
+            <div className="bg-neutral-800 w-full px-5 h-20 flex justify-center items-center font-semibold pb-0">
+                <h2 className="text-white lg:text-base md:text-sm text-xs text-center">Developed by Zahra Rostami, Sadra Yavarzadeh, Hossein Mohammadzadeh, Mahdi Hashemi, Sahar Fallahi, Ahmadreza Navaie, Mohammad Zorrie, Alireza Arjmandnia, <span className="text-gold">Your name place*</span></h2>
             </div>
         </>
     )
