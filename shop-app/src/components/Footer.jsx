e47df3076310da9d9bc285e5fc6efa13f81f5054
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-21 16:15:18 +0330

    edit Footer: add tester list with linkedin page link

--- shop-app/src/components/Footer.jsx
+++ shop-app/src/components/Footer.jsx
@@ -1,8 +1,27 @@
+import { testers } from "./TesterList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
     return (
         <>
-            <div className="bg-neutral-800 w-full px-5 h-20 flex justify-center items-center font-semibold pb-0">
-                <h2 className="text-white lg:text-base md:text-sm text-xs text-center">Developed by Zahra Rostami, Sadra Yavarzadeh, Hossein Mohammadzadeh, Mahdi Hashemi, Sahar Fallahi, Ahmadreza Navaie, Mohammad Zorrie, Alireza Arjmandnia, <span className="text-gold">Your name place*</span></h2>
+            <div className="bg-neutral-800 w-full px-5  py-3 mx-auto text-white">
+                <h2 className="lg:text-base md:text-sm text-xs text-center">Developed by <span className="font-bold">Zahra Rostami</span></h2>
+                <h2 className="lg:text-base md:text-sm text-xs text-center">Tested and Approved by </h2>
+                <div className="px-10">
+                    {
+                        testers.map((tester, index) => (
+                            <div className="flex flex-row justify-center">
+                                <h2 key={index} className="text-white text-xs md:text-sm">
+                                    {tester.name}
+                                </h2>
+                                <a href={tester.linkedIn}>
+                                    <FontAwesomeIcon icon={faUpRightFromSquare} />
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
