5e31a21926bf11677480943e7b80a9d663ff5b4d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-21 16:15:33 +0330

    add TesterList

---
+++ shop-app/src/components/TesterList.jsx
@@ -0,0 +1,30 @@
+export const testers = [
+    {
+        name: 'Sadra Yavarzadeh',
+        linkedIn: 'https://www.linkedin.com/in/sadrayavar',
+    },
+    {
+        name: 'Hossein Mohammadzadeh',
+        linkedIn: 'https://www.linkedin.com/in/hossein-mohammadzadeh-a847542b2'
+    },
+    {
+        name: ' Mahdi Hashemi',
+        linkedIn: 'https://www.linkedin.com/in/s-m-hashemi',
+    },
+    {
+        name: 'Sahar Fallahi',
+        linkedIn: 'https://www.linkedin.com/in/Sahar-fallahi',
+    },
+    {
+        name: 'Ahmadreza Navaie',
+        linkedIn: 'https://www.linkedin.com/ahmadreza-navaie-033813214',
+    },
+    {
+        name: 'Mohammad Zorrie',
+        linkedIn: 'https://www.linkedin.com/mohammad-zorrie-871aa91a8',
+    },
+    {
+        name: 'Alireza Arjmandnia',
+        linkedIn: 'https://www.linkedin.com/in/alireza-%E2%80%8C%E2%80%8C%E2%80%8C%E2%80%8Carjmandnia-537380340',
+    },
+]
