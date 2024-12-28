e17259420daffdb15d8c9742930f8873bb49a2f5
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-28 21:03:02 +0330

    edit main: change App and Store path

--- shop-app/src/main.jsx
+++ shop-app/src/main.jsx
@@ -1,7 +1,7 @@
-import { Store } from './redux/Store.jsx';
+import { Store } from './redux/Store';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
