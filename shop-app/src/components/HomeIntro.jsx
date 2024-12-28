68f05158dea5a25cddb881df1ad31d4eb5d79b28
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-28 12:27:37 +0330

    Add HomeIntro components for display shop introducttion

---
+++ shop-app/src/components/HomeIntro.jsx
@@ -0,0 +1,31 @@
+const HomeIntro = () => {
+    return (
+        <>
+            <p className="lg:my-20 md:my-14 my-2 lg:text-xl md:text-xl text-xs font-semibold text-gray-700 px-5">
+                <p>
+                    Discover a world of endless possibilities at ChicMarket, where we bring you the finest selection of products to suit every lifestyle and need. Whether you’re looking for stylish apparel, cutting-edge electronics, or timeless jewelry, we’ve got it all under one roof!
+                </p>
+                <p className="lg:my-10 md:my-14 my-2 lg:text-xl md:text-xl text-xs font-semibold text-gray-700 ">
+                    Fashion for Every Occasion:
+                    Explore our exclusive collection of women’s and men’s clothing, featuring the latest trends in casual wear, formal attire, and activewear. From chic dresses and tailored suits to comfortable t-shirts and cozy loungewear, redefine your wardrobe with styles that inspire confidence and individuality.
+                </p>
+                <p className="lg:my-10 md:my-14 my-2 lg:text-xl md:text-xl text-xs font-semibold text-gray-700 ">
+                    Tech That Powers Your Life:
+                    Stay ahead of the curve with our premium range of electronics. From high-performance hard drives and sleek laptops to smart TVs and gadgets that make everyday life easier, we bring technology closer to you.
+                </p>
+                <p className="lg:my-10 md:my-14 my-2 lg:text-xl md:text-xl text-xs font-semibold text-gray-700 ">
+                    Jewelry That Dazzles:
+                    Elevate your look with our stunning array of jewelry. From elegant necklaces and bracelets to statement rings and earrings, our timeless pieces are perfect for celebrating life’s special moments or adding sparkle to your everyday ensemble.
+                </p>
+                <p className="lg:my-10 md:my-14 my-2 lg:text-xl md:text-xl text-xs font-semibold text-gray-700 ">
+                    At ChicMarket, we’re committed to delivering quality, variety, and convenience. Whether you're shopping for yourself or looking for the perfect gift, we’ve curated something extraordinary just for you.
+                </p>
+                <p className="lg:my-20 md:my-14 my-2 lg:text-xl md:text-xl text-xs font-semibold text-gray-700 ">
+                    Shop with us today and experience the difference!
+                </p>
+            </p>
+        </>
+    );
+}
+
+export default HomeIntro;
