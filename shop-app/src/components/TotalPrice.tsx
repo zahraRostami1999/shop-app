c2f015bf5f15eaabe553fcc515d9b133a9312af3
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-29 19:01:08 +0330

    add TotalPrice for the current transaction TotalPrice

---
+++ shop-app/src/components/TotalPrice.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface CartItem {
+  id: number;
+  qty: number;
+  price: number;
+}
+
+const TotalPrice: React.FC = () => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItem") as string);
+
+  let total = 0;
+  cartItems.map((item: CartItem) => {
+    total += item.qty * item.price;
+  });
+  return (
+    <p>
+      Your total Price is <span className="text-green-700">€{total}</span>
+    </p>
+  );
+};
+
+export default TotalPrice;
