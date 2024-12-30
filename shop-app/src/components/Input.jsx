e79bc5a677ba85753d0bb0bb08ae201675f69491
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-30 14:59:30 +0330

    edit Input location

---
+++ shop-app/src/components/Input.jsx
@@ -0,0 +1,15 @@
+
+function Input(props){
+
+return(
+    <div className="my-8 md:my-3 w-full  flex justify-between items-center pr-5">
+    <label className="lg:w-1/4 text-sm lg:text-lg">{props.label} </label>
+    <input
+      className="lg:w-2/3 w-2/3 h-6 border lg:h-9 rounded lg:rounded-lg lg:border-2 border-zinc-200 lg:px-3 focus:border-zinc-400"
+      type="text"
+    />
+  </div>
+);
+}
+
+export default Input;
