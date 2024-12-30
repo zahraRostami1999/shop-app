402de49e855ca29516af996afe44e1b69c574faa
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-30 19:31:16 +0330

    edit ConfirmOrder: add OrangeBtn and GrayBtn

--- shop-app/src/components/ConfirmOrder.tsx
+++ shop-app/src/components/ConfirmOrder.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import TotalPrice from "./TotalPrice";
 import { Link } from "react-router-dom";
-
+import OrangeBtn from "./Btn/OrangeBtn";
+import GrayBtn from "./Btn/GrayBtn";
 const ConfirmOrder: React.FC = () => {
   return (
-    <div className="lg:text-xl md:text-xl text-base font-Poppins font-semibold flex flex-wrap lg:w-10/12 md:w-10/12 w-1/2 justify-between items-center pb-5 mb-10 border-gray-200 border-2 shadow-md lg:px-5 px-0 py-3 rounded-lg flex-grow">
-      <div>
+    <div className="lg:w-1/2 md:w-1/2 lg:text-2xl md:text-xl w-full text-base font-Poppins font-semibold flex lg:mx-10 md:mx-7 justify-center items-center pb-5 mb-10 border-gray-200 border-2 shadow-md lg:px-5 px-5 py-3 rounded-lg flex-wrap">
+      <div className="sm:w-full w-full text-center px-3">
         <TotalPrice />
       </div>
-      <div className="lg:w-1/2 md:w-2/3 w-11/12 px-2 flex justify-around mx-auto">
+      <div className="lg:w-full md:w-4/5 sm:w-10/12 w-2/3 px-2 flex justify-center mx-auto py-3">
         <Link to="/Checkout">
-          <button className="bg-orange-500 hover:scale-105 transition duration-100 ease-in-out text-zinc-900 lg:text-lg md:text-lg text-xs text-center font-bold py-3 lg:px-5 md:px-5 px-3 rounded-xl mt-6">
-            Proceed to checkout
-          </button>
+          <OrangeBtn content="Checkout" />
         </Link>
         <Link to="/Products">
-          <button className="bg-gray-300 lg:hover:scale-105 hover:bg-gray-400 transition duration-100 ease-in-out text-zinc-900 lg:text-lg md:text-lg text-xs text-center font-bold py-3 lg:px-10 md:px-5 px-3 rounded-xl mt-6">
-            Go back to store
-          </button>
+          <GrayBtn content="Store" />
         </Link>
       </div>
     </div>
