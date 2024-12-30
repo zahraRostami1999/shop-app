8331c73012b8e9b04d14b67302d4d647718e37e4
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-30 14:58:16 +0330

    edit App: change Header path

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 const LazyCart = React.lazy(() => import('./pages/CartPage'));
 const LazyUndefined = React.lazy(() => import('./pages/Page404'));
