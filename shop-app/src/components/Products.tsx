dcb9c26431ea488f92d630469d12569dc78aa993
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-30 21:44:11 +0330

    convert Products to tsx

--- shop-app/src/components/Products.tsx
+++ shop-app/src/components/Products.tsx
@@ -1,24 +1,40 @@
-import { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Card from "./Card";
 import { getProducts } from "../services/api";
-import React from "react";
-import BackTop from './BackTop';
+import BackTop from "./BackTop";
 import Spinner from "./Spinner";
 
-function Products() {
-  const [productData, setProductData] = useState([]);
-  const [filter, setFilter] = useState("all");
-  const filterBtn = ["all", "men's clothing", "women's clothing", "jewelery", "electronics"];
-  const [isLoading, setIsLoading] = useState(true);
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+  category: string;
+}
+
+const Products: React.FC = () => {
+  const [productData, setProductData] = useState<Product[]>([]);
+  const [filter, setFilter] = useState<string>("all");
+  const filterBtn = [
+    "all",
+    "men's clothing",
+    "women's clothing",
+    "jewelery",
+    "electronics",
+  ];
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     getProducts()
-      .then((result) => { setProductData(result); setIsLoading(false); })
+      .then((result) => {
+        setProductData(result);
+        setIsLoading(false);
+      })
       .catch((error) => console.log(error));
   }, []);
 
-
-  const handleFilterProducts = (newFilter) => {
+  const handleFilterProducts = (newFilter: string) => {
     setFilter(newFilter);
   };
 
@@ -29,11 +45,21 @@ function Products() {
 
   return (
     <>
-      {isLoading ? <Spinner /> : (
+      {isLoading ? (
+        <Spinner />
+      ) : (
         <div className="min-h-screen">
           <div className="lg:mt-20 md:mt-20 mt-16 flex flex-wrap border-b justify-start px-2 lg:px-16 md:px-12 sm:px-5 text-xs lg:text-base md:text-base font-Poppins font-medium">
             {filterBtn.map((btn) => {
-              return <button className="px-2 lg:px-5 md:px-3 py-2  hover:text-neutral-600 hover:border-b hover:border-neutral-900 transition-all duration-300" key={btn} onClick={() => handleFilterProducts(btn)}>{btn}</button>
+              return (
+                <button
+                  className="px-2 lg:px-5 md:px-3 py-2  hover:text-neutral-600 hover:border-b hover:border-neutral-900 transition-all duration-300"
+                  key={btn}
+                  onClick={() => handleFilterProducts(btn)}
+                >
+                  {btn}
+                </button>
+              );
             })}
           </div>
           <div className="w-5/6 lg:w-11/12 md:w-5/6 sm:w-11/12 mx-auto mb-24 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 gap-3">
@@ -48,5 +74,5 @@ function Products() {
       )}
     </>
   );
-}
+};
 export default Products;
