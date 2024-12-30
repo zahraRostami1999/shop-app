b0129e2b0a4ed05ef96ef473117b38a73f5c9f11
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-30 21:44:38 +0330

    convert CartPage to tsx

--- shop-app/src/pages/CartPage.tsx
+++ shop-app/src/pages/CartPage.tsx
@@ -1,17 +1,18 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import ConfirmOrder from "../components/ConfirmOrder";
 import DisplaycartItems from "../components/DisplaycartItems";
 import EmptyCartPage from "./EmptyCartPage";
+import { RootState } from "../redux/store";
 
 function CartPage() {
-  const { cartItems } = useSelector((state) => state.cart);
-
+  const cartItems = useSelector((state: RootState) => state.cart.cartItems);
   return (
     <>
       <div className=" min-h-screen w-full flex justify-center items-center flex-wrap">
         <ul className="flex justify-center flex-wrap">
-          {cartItems.length > 0 ? < DisplaycartItems /> : <EmptyCartPage />}
-          {cartItems.length > 0 ? <ConfirmOrder /> : console.log(cartItems.length)}
+          {cartItems.length > 0 ? <DisplaycartItems /> : <EmptyCartPage />}
+          {cartItems.length > 0 ? <ConfirmOrder /> : null}
         </ul>
       </div>
     </>
