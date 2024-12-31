f3a2f4466a2a8b43abe943d1ab78a566779bbe78
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-31 22:37:40 +0330

    edit CartPage: add ConfirmOrder

--- shop-app/src/pages/CartPage.tsx
+++ shop-app/src/pages/CartPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import ConfirmOrder from "../components/ConfirmOrder";
 import DisplaycartItems from "../components/DisplaycartItems";
 import EmptyCartPage from "./EmptyCartPage";
 import { RootState } from "../redux/store";
@@ -11,6 +12,7 @@ function CartPage() {
       <div className=" min-h-screen w-full flex justify-center items-center flex-wrap">
         <ul className="flex justify-center flex-wrap">
           {cartItems.length > 0 ? <DisplaycartItems /> : <EmptyCartPage />}
+          {cartItems.length > 0 ? <ConfirmOrder /> : null}
         </ul>
       </div>
     </>
