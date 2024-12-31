803808b143885409a1d4c8c799611a317cc69665
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-31 22:34:53 +0330

    edit ConfirmOrder: add OrangeBtn and  grayBtn

--- shop-app/src/components/ConfirmOrder.tsx
+++ shop-app/src/components/ConfirmOrder.tsx
@@ -10,11 +10,11 @@ const ConfirmOrder: React.FC = () => {
         <TotalPrice />
       </div>
       <div className="lg:w-full md:w-4/5 sm:w-10/12 w-2/3 px-2 flex justify-center mx-auto py-3">
-        <Link to="/Checkout">
-          <OrangeBtn content="Checkout" />
+        <Link className="lg:w-36 lg:h-12 md:w-36 md:h-12 sm:w-24 sm:h-10 w-20 h-8 mx-3" to="/Checkout">
+          <OrangeBtn content="Checkout"/>
         </Link>
-        <Link to="/Products">
-          <GrayBtn content="Store" />
+        <Link className="lg:w-36 lg:h-12 md:w-36 md:h-12 sm:w-24 sm:h-10 w-20 h-8 mx-3" to="/Products">
+          <GrayBtn content="Store"/>
         </Link>
       </div>
     </div>
