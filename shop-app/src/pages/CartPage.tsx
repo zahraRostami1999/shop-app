f0474e3187bc122f4931517f547c8ad70cb0c4f7
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-01 19:10:43 +0330

    edit CartPage: import ShoppingCart

--- shop-app/src/pages/CartPage.tsx
+++ shop-app/src/pages/CartPage.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import ConfirmOrder from "../components/ConfirmOrder";
-import DisplaycartItems from "../components/DisplaycartItems";
 import EmptyCartPage from "./EmptyCartPage";
-import { RootState } from "../redux/store";
+import { RootState } from "../redux/Store";
+import ShoppingCart from "../components/ShoppingCart";
 
 function CartPage() {
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
   return (
     <>
       <div className=" min-h-screen w-full flex justify-center items-center flex-wrap">
-        <ul className="flex justify-center flex-wrap">
-          {cartItems.length > 0 ? <DisplaycartItems /> : <EmptyCartPage />}
-          {cartItems.length > 0 ? <ConfirmOrder /> : null}
+        <ul className="flex justify-center flex-wrap w-full">
+          {cartItems.length > 0 ? <ShoppingCart /> : <EmptyCartPage />}
         </ul>
       </div>
     </>
