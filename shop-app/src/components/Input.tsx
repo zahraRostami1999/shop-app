5b85a6f3bf652043810aad69c0ddaf9679d58f16
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-04 20:51:20 +0330

    edit Input: change label w to 1/2

--- shop-app/src/components/Input.tsx
+++ shop-app/src/components/Input.tsx
@@ -7,7 +7,7 @@ interface Label {
 const Input: React.FC<Label> = ({ label }) => {
   return (
     <div className="my-8 md:my-3 w-full  flex justify-between items-center pr-5">
-      <label className="lg:w-1/4 text-sm lg:text-lg">{label} </label>
+      <label className="w-1/2 text-xs lg:text-sm">{label} </label>
       <input
         className="lg:w-2/3 w-2/3 h-6 border lg:h-9 rounded lg:rounded-lg lg:border-2 border-zinc-200 lg:px-3 focus:border-zinc-400"
         type="text"
