70f2dbc3bfb5fba3c1ca68752c73a325f2963cbd
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-04 20:48:34 +0330

    edit Footer: add top border and change Tested title color to rose

--- shop-app/src/components/Footer.tsx
+++ shop-app/src/components/Footer.tsx
@@ -3,10 +3,10 @@ import { testers } from "./TesterList";
 const Footer:React.FC = () => {
     return (
         <>
-            <div className="bg-neutral-800 w-full lg:px-5 md:px-5 sm:px-1 px-1 py-3 mx-auto text-white">
-                <h2 className="lg:text-base md:text-sm text-sm text-center pb-2">Developed by <span className="font-bold">Zahra Rostami</span></h2>
+            <div className="bg-neutral-800 w-full lg:px-10 md:px-10 sm:px-5 px-5 py-5 mx-auto text-white ">
+                <h2 className="lg:text-base md:text-sm text-sm text-center py-5 border-white border-t-2">Developed by <span className="font-bold">Zahra Rostami</span></h2>
                 <div className="px-5 flex justify-start items-start flex-col ">
-                    <h2 className="lg:text-base md:text-sm text-sm text-center font-bold">Tested and Improved by </h2>
+                    <h2 className="lg:text-base md:text-sm text-sm text-center font-bold text-rose-500">Tested and Improved by </h2>
                     {
                         testers.map((tester, index) => (
                             <div key={index} className="text-left w-full xl:w-1/5 lg:w-1/4 md:w-1/2 sm:w-1/2 sm:text-xs lg:text-sm">
