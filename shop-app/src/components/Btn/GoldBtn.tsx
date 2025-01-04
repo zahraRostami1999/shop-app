b3cbc521c1d3c513f5107c1b65ad6e511790118d
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-04 13:13:23 +0330

    Add goldBtn

---
+++ shop-app/src/components/Btn/GoldBtn.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Btn {
+  children: React.ReactNode;
+}
+
+const GoldBtn: React.FC<Btn> = ({ children }) => {
+  return (
+    <>
+      <button className="w-full bg-gold lg:hover:scale-105 hover:scale-95 transition duration-100 ease-in-out text-zinc-900 lg:text-lg md:text-lg text-xs text-center font-bold rounded-md">
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default GoldBtn;
