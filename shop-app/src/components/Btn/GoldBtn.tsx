5ab9579be072b75f33c4b48fcea40813ac95f9ee
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-04 20:51:38 +0330

    edit GoldBtn: change bg to rose

--- shop-app/src/components/Btn/GoldBtn.tsx
+++ shop-app/src/components/Btn/GoldBtn.tsx
@@ -7,7 +7,7 @@ interface Btn {
 const GoldBtn: React.FC<Btn> = ({ children }) => {
   return (
     <>
-      <button className="w-full bg-gold lg:hover:scale-105 hover:scale-95 transition duration-100 ease-in-out text-zinc-900 lg:text-lg md:text-lg text-xs text-center font-bold rounded-md">
+      <button className="w-full bg-rose-400 lg:hover:scale-105 hover:scale-95 transition duration-100 ease-in-out text-zinc-900 lg:text-lg md:text-lg text-xs text-center font-bold rounded-md">
         {children}
       </button>
     </>
