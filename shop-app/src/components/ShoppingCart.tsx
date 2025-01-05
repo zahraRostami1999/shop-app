f609d488e1d4bfc502e717b9cb5a1a078d413f85
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-05 19:07:45 +0330

    feat(CartPage): Added "Your cart items" heading and "back to shop" link

--- shop-app/src/components/ShoppingCart.tsx
+++ shop-app/src/components/ShoppingCart.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import Spinner from "./Spinner";
 import DisplaycartItems from "./DisplaycartItems";
 import ConfirmOrder from "./ConfirmOrder";
 import { useGettingProducts } from "../services/Query";
 
 const ShoppingCart = () => {
-  const { isLoading: loading } = useGettingProducts();
-  return (
-    <>
-      {loading ? (
-        <div className="w-full">
-          <Spinner />
-        </div>
-      ) : (
-        <div className="">
-          <div className="w-full">
-            <DisplaycartItems />
-          </div>
-          <div className="w-full flex justify-center">
-            <ConfirmOrder />
-          </div>
-        </div>
-      )}
-    </>
-  );
+   const { isLoading: loading } = useGettingProducts();
+   return (
+      <>
+         {loading ? (
+            <div className="w-full">
+               <Spinner />
+            </div>
+         ) : (
+            <div className="w-full">
+               <div className="w-full  my-20">
+                  <div className="my-10">
+                     <h1 className="w-full my-3 lg:text-2xl md:text-xl sm:text-lg text-base font-semibold text-center">Your cart items</h1>
+                     <Link to="/Products"><h1 className="w-full lg:text-base font-semibold text-center text-rose-600">Back to shopping</h1></Link>
+                  </div>
+                  <div className="flex justify-center flex-wrap w-full">
+                     <DisplaycartItems />
+                  </div>
+               </div>
+               <div className="w-full flex justify-center my-44">
+                  <ConfirmOrder />
+               </div>
+            </div>
+         )}
+      </>
+   );
 };
 
 export default ShoppingCart;
