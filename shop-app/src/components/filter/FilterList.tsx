b22e6467c43f2467b24e84250720dadd2a36dc47
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-06 21:25:28 +0330

    feat(Products): Created a new FilterList component for filtering products by category, price, and rating.

---
+++ shop-app/src/components/filter/FilterList.tsx
@@ -0,0 +1,25 @@
+export const PriceFilterList = [
+   { label: "Price", value: "all" },
+   { label: "All", value: "all" },
+   { label: "Below €10", value: "10" },
+   { label: "€10 to €30", value: "30" },
+   { label: "€30 to €100", value: "100" },
+   { label: "Above €100", value: "+100" },
+];
+
+export const CategoryFilterList = [
+   { label: "Category", value: "all" },
+   { label: "All", value: "all" },
+   { label: "Men", value: "men's clothing" },
+   { label: "Women", value: "women's clothing" },
+   { label: "Jewelery", value: "jewelery" },
+   { label: "Electronics", value: "electronics" },
+];
+
+export const RatingFilterList = [
+   { label: "Rate", value: "all" },
+   { label: "All", value: "all" },
+   { label: "High", value: "4" },
+   { label: "Medium", value: "3" },
+   { label: "Low", value: "1" },
+];
