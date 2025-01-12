bf665cbdcc01cd2e2ae32049fc143f9aa6c0dcf6
Zahra Rostami <zahrarostami137878@gmail.com>
2025-01-12 19:26:04 +0330

    Move TesterList and FilterList files to the helpers directory

---
+++ shop-app/src/helpers/TesterList.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface Tester {
+    name: string,
+    linkedIn: string,
+}
+
+export const testers: Tester[] = [
+    {
+        name: 'Sadra Yavarzadeh',
+        linkedIn: 'https://www.linkedin.com/in/sadrayavar',
+    },
+    {
+        name: 'Hossein Mohammadzadeh',
+        linkedIn: 'https://www.linkedin.com/in/hossein-mohammadzadeh-a847542b2'
+    },
+    {
+        name: 'Mahdi Hashemi',
+        linkedIn: 'https://www.linkedin.com/in/s-m-hashemi',
+    },
+    {
+        name: 'Sahar Fallahi',
+        linkedIn: 'https://www.linkedin.com/in/Sahar-fallahi',
+    },
+    {
+        name: 'Ahmadreza Navaie',
+        linkedIn: 'https://www.linkedin.com/in/ahmadreza-navaie-033813214',
+    },
+    {
+        name: 'Mohammad Zorrie',
+        linkedIn: 'https://www.linkedin.com/in/mohammad-zorrie-871aa91a8',
+    },
+    {
+        name: 'Alireza Arjmandnia',
+        linkedIn: 'https://www.linkedin.com/in/alireza-%E2%80%8C%E2%80%8C%E2%80%8C%E2%80%8Carjmandnia-537380340',
+    },
+    {
+        name: 'Hassan Hamidi',
+        linkedIn: 'https://www.linkedin.com/in/hassan-hamidi',
+    },
+    {
+        name: 'Mahdi Solgi',
+        linkedIn: 'https://www.linkedin.com/in/mahdi-solgi',
+    },
+]
